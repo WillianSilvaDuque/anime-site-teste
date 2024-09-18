cfa24d16f6d093243d4329508a3f9da551a2b431
WillianSilvaDuque <114189970+WillianSilvaDuque@users.noreply.github.com>
2024-09-18 19:08:20 -0300

    Delete assets directory

--- assets/javascript/swiper.js
+++
@@ -1,1 +0,0 @@
-var swiper = new Swiper('.mySwiper', { slidesPerView: 'auto', loop: true, loopFillGroupWithBlank: true, spaceBetween: 20, breakpoints: { 320: { spaceBetween: 10 }, 480: { spaceBetween: 20 } } });
